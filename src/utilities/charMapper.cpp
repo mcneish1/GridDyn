a0d2038b4365c70b47d7e2e28c765c707d8c49a0
Corey McNeish <mcneish1@llnl.gov>
2019-09-22 18:56:32 -0700

    Delete more unused code

--- src/utilities/charMapper.cpp
+++ src/utilities/charMapper.cpp
@@ -14,28 +14,6 @@
 
 namespace utilities
 {
-charMapper<bool> numericMapper ()
-{
-    charMapper<bool> nm (false);
-    nm.addKey ('0', true);
-    nm.addKey ('1', true);
-    nm.addKey ('2', true);
-    nm.addKey ('3', true);
-    nm.addKey ('4', true);
-    nm.addKey ('5', true);
-    nm.addKey ('6', true);
-    nm.addKey ('7', true);
-    nm.addKey ('8', true);
-    nm.addKey ('9', true);
-    nm.addKey ('+', true);
-    nm.addKey ('-', true);
-    nm.addKey (' ', true);
-    nm.addKey ('e', true);
-    nm.addKey ('E', true);
-    nm.addKey ('.', true);
-    return nm;
-}
-
 charMapper<bool> numericStartMapper ()
 {
     charMapper<bool> nm (false);
@@ -79,65 +57,6 @@ charMapper<bool> numericEndMapper ()
     nm.addKey ('\r', true);
     nm.addKey ('\0', true);
     return nm;
-}
-
-charMapper<unsigned char> base64Mapper ()
-{
-    charMapper<unsigned char> b64 (0xFF);
-    unsigned char val = 0;
-    for (unsigned char c = 'A'; c <= 'Z'; ++c)
-    {
-        b64.addKey (c, val);
-        ++val;
-    }
-    for (unsigned char c = 'a'; c <= 'z'; ++c)
-    {
-        b64.addKey (c, val);
-        ++val;
-    }
-    for (unsigned char c = '0'; c <= '9'; ++c)
-    {
-        b64.addKey (c, val);
-        ++val;
-    }
-    b64.addKey ('+', val++);
-    b64.addKey ('/', val);
-    return b64;
-}
-
-charMapper<unsigned char> digitMapper ()
-{
-    charMapper<unsigned char> dMap (0xFF);
-    unsigned char val = 0;
-    for (unsigned char c = '0'; c <= '9'; ++c)
-    {
-        dMap.addKey (c, val);
-        ++val;
-    }
-    return dMap;
-}
-
-charMapper<unsigned char> hexMapper ()
-{
-    charMapper<unsigned char> dMap (0xFF);
-    unsigned char val = 0;
-    for (unsigned char c = '0'; c <= '9'; ++c)
-    {
-        dMap.addKey (c, val);
-        ++val;
-    }
-    for (unsigned char c = 'A'; c <= 'F'; ++c)
-    {
-        dMap.addKey (c, val);
-        ++val;
-    }
-    val = 10;
-    for (unsigned char c = 'a'; c <= 'f'; ++c)
-    {
-        dMap.addKey (c, val);
-        ++val;
-    }
-    return dMap;
 }
 
 charMapper<unsigned char> pairMapper ()
