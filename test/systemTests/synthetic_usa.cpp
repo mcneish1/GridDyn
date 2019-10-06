808949999c6c6773a9221fa1cabf4c513a683337
Corey McNeish <mcneish1@llnl.gov>
2019-10-05 18:03:03 -0700

    Add test for SyntheticUSA test file

---
+++ test/systemTests/synthetic_usa.cpp
@@ -0,0 +1,26 @@
+#ifndef GRIDDYN_TEST_DIRECTORY
+#error Please define GRIDDYN_TEST_DIRECTORY
+#endif
+
+#include <gtest/gtest.h>
+
+#include <filesystem>
+
+#include "fileInput/fileInput.h"
+#include "griddyn/gridDynSimulation.h"
+
+std::filesystem::path local_test_directory()
+{
+    return std::filesystem::path(GRIDDYN_TEST_DIRECTORY) / "ACTIVS" / "SyntheticUSA";
+}
+
+TEST(SyntheticUSA, Loads)
+{
+    auto test_file = local_test_directory() / "SyntheticUSA.EPC";
+
+    ASSERT_TRUE(std::filesystem::exists(test_file)) << "Could not find " << test_file;
+
+    auto sim = std::make_unique<griddyn::gridDynSimulation>();
+
+    griddyn::loadFile(sim.get(), test_file);
+}
